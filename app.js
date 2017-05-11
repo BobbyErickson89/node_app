e59241c048feb9d1a9764d39764703a774edb0f3
Bobby Erickson <roericks@cisco.com>
2017-05-11 11:22:14 -0400

    test commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// Routes
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
