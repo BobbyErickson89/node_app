e6f35c59e79e0911213100954f009f7613c9e6b9
Bobby Erickson <ericksonr07@gmail.com>
2017-05-12 15:54:51 -0400

    added node-mysql to package.json and made db connection variable in app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'testdb'
+});
 
 //Routes
 app.get('/', function(req, res){
