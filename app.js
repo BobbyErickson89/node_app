9b505b21211cae77ea2c5bc57c3728c00962a340
Bobby Erickson <ericksonr07@gmail.com>
2017-05-19 00:44:01 -0400

    ajax call
    Have successful ajax call to the post url

--- app.js
+++ app.js
@@ -2,21 +2,31 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'testdb'
+  database: 'workout'
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Enabling our js and css folders in /public
 app.use(express.static(__dirname + '/public'));
 
-//Routes
+// GET Routes
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+// POST Routes
+app.post('/post_exercise', function(req, res){
+  console.log(req.body);
+  res.send('complete');
+})
 
 app.listen(6900, function(){
   console.log('App running on port 6900');
--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,29 @@
-$('#exercise-submit').click(function(){
-  // var form_data = {
-  //   exercise_type: $('#exercise-type').val(),
-  //   exercise: $('#exercise').val(),
-  //   sets: $('#sets').val(),
-  //   reps: $('#reps').val(),
-  //   weight: $('#weight').val()
-  // }
+$('#exercise-submit').click(function(e){
+  e.preventDefault();
 
-  localStorage.setItem('exercise_type', $('#exercise-type').val());
-  localStorage.setItem('exercise', $('#exercise').val());
-  localStorage.setItem('sets', $('#sets').val());
-  localStorage.setItem('reps', $('#reps').val());
-  localStorage.setItem('weight', $('#weight').val());
+  var form_data = {
+    exercise_type: $('#exercise-type').val(),
+    exercise: $('#exercise').val(),
+    sets: $('#sets').val(),
+    reps: $('#reps').val(),
+    weight: $('#weight').val()
+  }
 
-  return false;
+  $.ajax({
+    url: "/post_exercise",
+    type: "POST",
+    data: JSON.stringify(form_data),
+    contentType: "application/json",
+    cache: false,
+    complete: function(){
+      console.log(form_data);
+      console.log('process complete');
+    },
+    success: function(data){
+      console.log('success!');
+    },
+    error: function(req, err){
+      console.log('error message: ' + err);
+    }
+  });
 });
